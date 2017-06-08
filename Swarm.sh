b74e0213175c80c15f050164a8f8c77e2a0dc27f
SugendhKGanga <SugendhKGanga@users.noreply.github.com>
2017-06-08 12:38:41 +0530

    Create Swarm.sh

--- Swarm.sh
+++ Swarm.sh
@@ -9,3 +9,6 @@ docker -H tcp://<node_1_ip>:2375 run -d swarm join --addr=<node1_ip>:2375 token:
 docker -H tcp://10.18.24.188:2375 run -d swarm join --addr=10.18.24.188:2375 token://a02205a0787be77522d2ba4c938df879
 
 docker -H tcp://10.18.24.192:2375 run -d swarm join --addr=10.18.24.192:2375 token://a02205a0787be77522d2ba4c938df879
+
+docker -H tcp://$MANAGER_HOST_IP:22222 run -itd --net swarm-network -e constraint:node==n1 --name u1 ubuntu
+docker -H tcp://$MANAGER_HOST_IP:22222 run -itd --net swarm-network -e constraint:node==n2 --name u2 ubuntu
